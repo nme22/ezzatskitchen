fbecec6283e662951c3b19133c14dfe44b6f65b7
Navid Ebrahimi <navidmebrahimi22@gmail.com>
2022-02-28 15:15:27 -0500

    added proper redirect for user session

--- components/Auth.js
+++ components/Auth.js
@@ -8,7 +8,12 @@ export default function Auth() {
    const handleLogin = async (email) => {
       try {
          setLoading(true);
-         const { error } = await supabase.auth.signIn({ email });
+         const { error } = await supabase.auth.signIn(
+            { email },
+            {
+               redirectTo: `http://localhost:3000/Login`,
+            }
+         );
          if (error) throw error;
          alert('Check your email for the login link!');
       } catch (error) {
