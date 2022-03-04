07d7bc4b9bdd8ce154bba8c9c2a3ca3aad2a7452
Navid Ebrahimi <navidmebrahimi22@gmail.com>
2022-03-03 20:18:53 -0500

    worked on about page a little

--- pages/About.js
+++ pages/About.js
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import Image from 'next/image';
+
 export default function About() {
    return (
       <main>
@@ -6,21 +8,31 @@ export default function About() {
             <Link href="/Login">Login</Link>
             <Link href="/">Back</Link>
          </nav>
-         <article className="about">
-            <h1>A little about Ezzat</h1>
+         <div className="center">
+            <article className="about">
+               <h1 className="center">A little about Ezzat</h1>
+               <Image
+                  src="/baclava.jpeg"
+                  className="iranImage"
+                  width={200}
+                  height={200}
+                  alt="shiraz"
+               />
 
-            <p>
-               {' '}
-               Ezzat is an Iranian immigrant from Abadan, Iran. She became an
-               American citizen in the 1980's and ended up settling in Queens,
-               New York. There created a loving family and two first generations
-               of Iranian Americans. While raising her kids, she noticed the
-               American culture tends to lean towards foods with high
-               cholesterol and trans fats at ridiculous prices.Using various
-               recipes from her childhood, lineage, and experiences; Ezzat aims
-               to bring you authentic persian cuisine at an affordable price.
-            </p>
-         </article>
+               <p>
+                  {' '}
+                  Ezzat is an Iranian immigrant from Abadan, Iran. She became an
+                  American citizen in the 1980's and ended up settling in
+                  Queens, New York. There created a loving family and two first
+                  generations of Iranian Americans. While raising her kids, she
+                  noticed the American culture tends to lean towards foods with
+                  high cholesterol and trans fats at ridiculous prices.Using
+                  various recipes from her childhood, lineage, and experiences;
+                  Ezzat aims to bring you authentic persian cuisine at an
+                  affordable price.
+               </p>
+            </article>
+         </div>
       </main>
    );
 }
